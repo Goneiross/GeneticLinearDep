9b8b32ffc66a0f80b4b2d11b075a2837c71010f5
Goneiross <guillaume.julien.pellerin@gmail.com>
2018-12-03 15:38:15 +0100

    Add prototype

---
+++ main.js
@@ -0,0 +1,30 @@
+function ini() {
+
+}
+
+function evaluation() {
+
+}
+
+function selection() {
+
+}
+
+function reproduction() {
+
+}
+
+function eugenisme() {
+
+}
+
+function deces() {
+
+}
+
+function main() {
+
+}
+
+function analysis() {
+}
